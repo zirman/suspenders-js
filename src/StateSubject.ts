aacd62a2a1a154c34b8ae8b939018b8e65827cbf
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2021-02-07 14:01:25 -0500

    Removed comment

--- src/StateSubject.ts
+++ src/StateSubject.ts
@@ -3,13 +3,6 @@ import { Scope } from "./Scope.js";
 import { Observer } from "./Types.js";
 
 /**
- * Subjects are "hot" because they are inputs for events and state into that exists whether or not
- * it is being observed. produce values even if there are no observers listening for
- * updates. Subjects should be used for injecting event based data like mouse clicks or continuous
- * data like mouse position.
- */
-
-/**
  * StateSubject always have a value. When new observers are added, the last emitted value is
  * replayed. This is generally used used for hot observables like the mouse position. Subjects are
  * hot and can be shared with multipler observers. New flows that observe subjects start cold.
